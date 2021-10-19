e9501c7af09bacc5f2c0743a9bda88e10ee0db97
christineiym <christine.mendoza@unc.edu>
2021-10-18 22:18:23 -0400

    add H- and -OH to ends of peptides

--- utils_amino_acids_to_chemical_formula.py
+++ utils_amino_acids_to_chemical_formula.py
@@ -30,14 +30,9 @@ def sequence_to_atom_totals(sequence: str) -> dict[str, int]:
             else:
                 atom_totals.update({atom : chemical_composition[atom]})
     
-    # oxygens_to_remove: int = len(sequence) - 1
-    # hydrogens_to_remove: int = 2 * oxygens_to_remove
-
-    # try:
-    #     atom_totals.update({'O' : (atom_totals['O'] - oxygens_to_remove)})
-    #     atom_totals.update({'H' : (atom_totals['H'] - hydrogens_to_remove)})
-    # except:
-    #     return {}
+    # Add H- and -OH
+    atom_totals.update({'O' : (atom_totals['O'] + 1)})
+    atom_totals.update({'H' : (atom_totals['H'] + 2)})
     
     return atom_totals
 
